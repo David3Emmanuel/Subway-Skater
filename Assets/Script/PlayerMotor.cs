0a0d2da6575fda87eb86426431df59247e9a6b64
David Emmanuel <david3emmanuel@gmail.com>
2024-10-22 15:53:51 +0100

    Animations and Physics

--- Assets/Script/PlayerMotor.cs
+++ Assets/Script/PlayerMotor.cs
@@ -10,33 +10,84 @@ public class PlayerMotor : MonoBehaviour
     private float gravity = 12.0f;
     private float verticalVelocity;
     private float speed = 7.0f;
+    private const float TURN_SPEED = 0.05f;
 
     // Lane
     private int desiredLane = 1; // 0 = Left, 1 = Middle, 2 = Right
     private const float LANE_DISTANCE = 3.0f;
 
-    void Start() {
+    // Animator
+    private Animator animator;
+
+    void Start()
+    {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
-    void Update() {
+    void Update()
+    {
         if (Input.GetKeyDown(KeyCode.LeftArrow)) MoveLane(false);
         if (Input.GetKeyDown(KeyCode.RightArrow)) MoveLane(true);
 
-        Vector3 targetPosition = new Vector3(0, 0, transform.position.z);
+        Vector3 targetPosition = new(0, 0, transform.position.z);
         if (desiredLane == 0) targetPosition += Vector3.left * LANE_DISTANCE;
         else if (desiredLane == 2) targetPosition += Vector3.right * LANE_DISTANCE;
 
         Vector3 moveVector = Vector3.zero;
         moveVector.x = (targetPosition - transform.position).normalized.x * speed;
-        moveVector.y = -0.1f;
+
+        bool isGrounded = IsGrounded();
+        animator.SetBool("Grounded", isGrounded);
+
+        if (isGrounded)
+        {
+            verticalVelocity = -0.1f;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                animator.SetTrigger("Jump");
+                verticalVelocity = jumpForce;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity -= jumpForce;
+            }
+        }
+        moveVector.y = verticalVelocity;
         moveVector.z = speed;
 
         controller.Move(moveVector * Time.deltaTime);
+        Vector3 direction = controller.velocity;
+        if (direction != Vector3.zero)
+        {
+            direction.y = 0;
+            transform.forward = Vector3.Lerp(transform.forward, direction.normalized, TURN_SPEED);
+        }
     }
 
-    void MoveLane(bool isGoingRight) {
+    void MoveLane(bool isGoingRight)
+    {
         desiredLane += isGoingRight ? 1 : -1;
         desiredLane = Mathf.Clamp(desiredLane, 0, 2);
+    }
+
+    bool IsGrounded()
+    {
+        Ray groundRay = new(
+            new Vector3(
+                controller.bounds.center.x,
+                controller.bounds.center.y - controller.bounds.extents.y + 0.2f,
+                controller.bounds.center.z
+            ),
+            Vector3.down
+        );
+
+        Debug.DrawRay(groundRay.origin, groundRay.direction, Color.cyan, 1.0f);
+        return Physics.Raycast(groundRay, 0.2f + 0.1f);
     }
 }
